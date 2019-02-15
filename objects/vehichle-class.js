bfe5ce95b5edff6b3d3793688da463d815107aff
RickBell <richard.belamy123@gmail.com>
2019-02-14 19:35:13 -0800

    initial commit

---
+++ objects/vehichle-class.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Car extends Vehichle {
+    constructor(name, wheels){
+        super(name, wheels);
+    }
+};
+
+class Motorcycle extends Vehichle {
+    constructor(name, wheels){
+        super(name, wheels);
+    }
+}
+
+module.exports = Car;
+module.exports = Motorcycle;
